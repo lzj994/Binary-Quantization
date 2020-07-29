1c0c1274d7ace167dc89563ba8e017b9762aeccf
Akaza994 <53023293+Akaza994@users.noreply.github.com>
2020-07-28 22:37:22 -0700

    Add files via upload

---
+++ quantization.py
@@ -0,0 +1,330 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 25 20:11:45 2020
+
+@author: zhijianli
+"""
+
+# -*- coding: utf-8 -*-
+"""
+main pgd enresnet
+"""
+import argparse
+import os
+import shutil
+import time
+
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+import torch
+import torch.nn as nn
+import math
+
+from resnet_cifar import *
+from utils import *
+
+def _weights_init(m):
+    classname = m.__class__.__name__
+    #print(classname)
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+        init.kaiming_normal_(m.weight)
+
+class LambdaLayer(nn.Module):
+    def __init__(self, lambd):
+        super(LambdaLayer, self).__init__()
+        self.lambd = lambd
+
+    def forward(self, x):
+        return self.lambd(x)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, option='A'):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            if option == 'A':
+                """
+                For CIFAR10 ResNet paper uses option A.
+                """
+                self.shortcut = LambdaLayer(lambda x:
+                                            F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
+            elif option == 'B':
+                self.shortcut = nn.Sequential(
+                     nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                     nn.BatchNorm2d(self.expansion * planes)
+                )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 16
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        self.linear = nn.Linear(64, num_classes)
+
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = F.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def resnet20():
+    return ResNet(BasicBlock, [3, 3, 3])
+
+def get_model2(model, learning_rate=1e-3, weight_decay=1e-4):
+
+    # set the first layer not trainable
+    # model.features.conv0.weight.requires_grad = False
+
+    # all fc layers
+    weights = [
+        p for n, p in model.named_parameters()
+        if 'weight' in n and 'conv' not in n
+    ]
+
+    # all conv layers
+    weights_to_be_quantized = [
+        p for n, p in model.named_parameters()
+        # if 'conv' in n and 'conv0' not in n
+        if 'conv' in n and 'weight' in n
+    ]
+
+    biases = [
+        p for n, p in model.named_parameters()
+        if 'bias' in n
+    ]    
+
+    params = [
+        {'params': weights, 'weight_decay': weight_decay},
+        {'params': weights_to_be_quantized, 'weight_decay': weight_decay},
+        {'params': biases,  'weight_decay': weight_decay}
+    ]
+    optimizer = optim.SGD(params, lr=learning_rate, momentum=0.9)
+
+    loss = nn.CrossEntropyLoss().cuda()
+    model = model.cuda()  # move the model to gpu
+    return model, loss, optimizer
+def quantize_bw(kernel):
+    """
+    binary quantization
+    Return quantized weights of a layer.
+    """
+    delta = kernel.abs().mean()
+    sign = kernel.sign().float()
+
+
+
+    return sign*delta
+
+net=resnet20().cuda()
+if __name__ == '__main__':
+    use_cuda = torch.cuda.is_available
+    global best_acc
+    best_acc = 0
+    start_epoch = 0
+    best_count = 0
+    #--------------------------------------------------------------------------
+    # Load Cifar data
+    #--------------------------------------------------------------------------
+    print('==> Preparing data...')
+    root = './data'
+    download = True
+    
+    #normalize = transforms.Normalize(mean=[0.507, 0.487, 0.441], std=[0.267, 0.256, 0.276])
+    
+    
+    train_set = torchvision.datasets.CIFAR10(
+        root=root,
+        train=True,
+        download=download,
+        transform=transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            #normalize,
+        ]))
+    
+    test_set = torchvision.datasets.CIFAR10(
+        root=root,
+        train=False,
+        download=download,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+            #normalize,
+        ]))
+    
+    
+    kwargs = {'num_workers':1, 'pin_memory':True}
+    batchsize_test = len(test_set)/40 #100
+    print('Batch size of the test set: ', batchsize_test)
+    test_loader = torch.utils.data.DataLoader(dataset=test_set,
+                                              batch_size=batchsize_test,
+                                              shuffle=False, **kwargs
+                                             )
+    batchsize_train = 128
+    print('Batch size of the train set: ', batchsize_train)
+    train_loader = torch.utils.data.DataLoader(dataset=train_set,
+                                               batch_size=batchsize_train,
+                                               shuffle=True, **kwargs
+                                              )
+    
+    net, criterion, optimizer = get_model2(net, learning_rate=0.1, weight_decay=5e-4)
+    #all_G_kernels=ckpt['G_kernels']
+    #m=ckpt['epoch']
+    
+    all_G_kernels = [
+        Variable(kernel.data.clone(), requires_grad=True)
+        for kernel in optimizer.param_groups[1]['params']
+    ]
+
+
+    all_W_kernels = [kernel for kernel in optimizer.param_groups[1]['params']]
+    kernels = [{'params': all_G_kernels}]
+    optimizer_quant = optim.SGD(kernels, lr=0)
+    eta_rate = 1.05
+    eta = 1
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[80,120,160], gamma=0.1)
+
+    
+    nepoch = 200
+    for epoch in xrange(nepoch):
+        print('Epoch ID', epoch)
+        #----------------------------------------------------------------------
+        # Training
+        #----------------------------------------------------------------------
+        correct = 0; total = 0; train_loss = 0
+        net.train()
+        for batch_idx, (x, target) in enumerate(train_loader):
+          #if batch_idx < 1:
+            optimizer.zero_grad()
+            x, target = Variable(x.cuda()), Variable(target.cuda())
+            all_W_kernels = optimizer.param_groups[1]['params']
+            all_G_kernels = optimizer_quant.param_groups[0]['params']
+            
+            for i in range(len(all_W_kernels)):
+                k_W = all_W_kernels[i]
+                k_G = all_G_kernels[i]
+                V = k_W.data
+                #k_G.data = quantize_bw(V)
+                
+                if epoch<120:
+                    k_G.data = (eta*quantize_bw(V)+V)/(1+eta)
+                    
+                else:
+                    k_G.data = quantize_bw(V)
+                   
+                k_W.data, k_G.data = k_G.data, k_W.data
+                
+            score= net(x)
+            loss = criterion(score, target)
+            loss.backward()
+            
+            for i in range(len(all_W_kernels)):
+                k_W = all_W_kernels[i]
+                k_G = all_G_kernels[i]
+                k_W.data, k_G.data = k_G.data, k_W.data
+            
+            optimizer.step()
+            
+            train_loss += loss.data
+            _, predicted = torch.max(score.data, 1)
+            total += target.size(0)
+            correct += predicted.eq(target.data).cpu().sum()
+            progress_bar(batch_idx, len(train_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            
+        #----------------------------------------------------------------------
+        # Testing
+        #----------------------------------------------------------------------
+        test_loss = 0; correct = 0; total = 0
+        net.eval()
+        
+        for i in range(len(all_W_kernels)):
+            k_W = all_W_kernels[i]
+            k_quant = all_G_kernels[i]    
+            k_W.data, k_quant.data = k_quant.data, k_W.data
+            
+        for batch_idx, (x, target) in enumerate(test_loader):
+            x, target = Variable(x.cuda(), volatile=True), Variable(target.cuda(), volatile=True)
+            score= net(x)
+            
+            loss = criterion(score, target)
+            test_loss += loss.data
+            _, predicted = torch.max(score.data, 1)
+            total += target.size(0)
+            correct += predicted.eq(target.data).cpu().sum()
+            progress_bar(batch_idx, len(test_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        
+        #----------------------------------------------------------------------
+        # Save the checkpoint
+        #----------------------------------------------------------------------
+        '''
+        for i in range(len(all_W_kernels)):
+            k_W = all_W_kernels[i]
+            k_quant = all_G_kernels[i]    
+            k_W.data, k_quant.data = k_quant.data, k_W.data
+          '''  
+        acc = 100.*correct/total
+        #if acc > best_acc:
+        if correct > best_count:
+            print('Saving model...')
+            state = {
+                'state': net.state_dict(), #net,
+                'acc': acc,
+                'epoch': epoch,
+            }
+            
+            torch.save(state, './resnet20.pth')
+            #net.save_state_dict('resnet20.pt')
+            best_acc = acc
+            best_count = correct
+
+        for i in range(len(all_W_kernels)):
+            k_W=all_W_kernels[i]
+            k_quant=all_W_kernels[i]
+            k_W.data, k_quant.data =k_quant.data,k_W.data
+    
+    print('The best acc: ', best_acc)
